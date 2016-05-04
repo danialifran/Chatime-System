5bc8f742c9aae3c86a6e90b2429712ba92e68bd9
[danialifran] <[danialifran@gmail.com]>
2016-05-04 12:23:34 +0800

    Login from database successfully but cannot passing parameter

--- Form2.cs
+++ Form2.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,29 +21,65 @@ namespace Mini_Project___Chatime
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            String username, password;
-            int id;
+           
 
-            username = txtUsername.Text;
-            password = txtPassword.Text;
+            // Create connection SQL Server 2012 
+            SqlConnection conn = new SqlConnection(@"Data Source =(LocalDB)\v11.0; 
+                            AttachDbFileName = |DataDirectory|ChatimeDB.mdf; 
+                            Integrated Security=true;");
 
-            if(username == "danial" && password == "1234")
+            conn.Open();
+            string checklogin = "select count(*) from users where login='" + txtUsername.Text + "'";
+            SqlCommand com = new SqlCommand(checklogin, conn);
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            conn.Close();
+            if (temp == 1)
             {
-                id = 5213142;
-                frmCashier frmCashier = new frmCashier(username,id);
-                frmCashier.Show();
-                this.Hide();
-            }
-            else if (username == "Karim" && password == "qwe123")
-            {
-                id = 52213150;
-                frmCashier frmCashier = new frmCashier(username, id);
-                frmCashier.Show();
+                conn.Open();
+                string checkpassword = "select password from users where login='" + txtUsername.Text + "'";
+                SqlCommand passCom = new SqlCommand(checkpassword, conn);
+                string password = passCom.ExecuteScalar().ToString().Replace(" ","");
+
+                if(password == txtPassword.Text)
+                {
+                    //Session ["New"] = txtUsername.Text;
+                    frmCashier frmCashier = new frmCashier();
+                    frmCashier.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username or Password");
+                }
             }
             else
             {
-                MessageBox.Show("Invalid Username or Password");
+                MessageBox.Show("Invalid input or username not been registerd");
             }
+
+            //String username, password;
+            //int id;
+
+            //username = txtUsername.Text;
+            //password = txtPassword.Text;
+
+            //if(username == "danial" && password == "1234")
+            //{
+            //    id = 5213142;
+            //    frmCashier frmCashier = new frmCashier(username,id);
+            //    frmCashier.Show();
+            //    this.Hide();
+            //}
+            //else if (username == "Karim" && password == "qwe123")
+            //{
+            //    id = 52213150;
+            //    frmCashier frmCashier = new frmCashier(username, id);
+            //    frmCashier.Show();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Invalid Username or Password");
+            //}
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
